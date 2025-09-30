4b621cf62a82170711bc6d72b9f295e36755c4f2
juhay-git <juha.yli-hemminki@seamk.fi>
2025-09-30 10:36:59 +0300

    alkupiste pelille ja taskilista

---
+++ matopeli.py
@@ -0,0 +1,76 @@
+# 'pip install PySide6' tarvitaan 
+import sys
+import random
+from PySide6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QMenu
+from PySide6.QtGui import QPainter, QPen, QBrush, QFont
+from PySide6.QtCore import Qt, QTimer
+
+# vakiot
+CELL_SIZE = 20
+GRID_WIDTH = 20
+GRID_HEIGHT = 15
+
+class SnakeGame(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+
+        self.setScene(QGraphicsScene(self))
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setSceneRect(0, 0, CELL_SIZE * GRID_WIDTH, CELL_SIZE * GRID_HEIGHT)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_game)
+        
+        self.start_game()
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key in (Qt.Key_Left, Qt.Key_Right, Qt.Key_Up, Qt.Key_Down):
+            # päivitetään suunta vain jos se ei ole vastakkainen valitulle suunnalle
+            if key == Qt.Key_Left and self.direction != Qt.Key_Right:
+                self.direction = key
+            elif key == Qt.Key_Right and self.direction != Qt.Key_Left:
+                self.direction = key
+            elif key == Qt.Key_Up and self.direction != Qt.Key_Down:
+                self.direction = key
+            elif key == Qt.Key_Down and self.direction != Qt.Key_Up:
+                self.direction = key
+
+    def update_game(self):
+        head_x, head_y = self.snake[0]
+
+        if self.direction == Qt.Key_Left:
+            new_head = (head_x - 1, head_y)
+        elif self.direction == Qt.Key_Right:
+            new_head = (head_x + 1, head_y)
+        elif self.direction == Qt.Key_Up:
+            new_head = (head_x, head_y - 1)
+        elif self.direction == Qt.Key_Down:
+            new_head = (head_x, head_y + 1)
+
+        self.snake.insert(0, new_head)
+        
+        self.snake.pop()
+
+        self.print_game()
+
+    def print_game(self):
+        self.scene().clear()
+
+        for segment in self.snake:
+            x, y = segment
+            self.scene().addRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, QPen(Qt.black), QBrush(Qt.black))
+        
+    def start_game(self):
+        self.direction = Qt.Key_Right
+        self.snake = [(5, 5), (5, 6), (5, 7)]
+        self.timer.start(300)
+
+def main():
+    app = QApplication(sys.argv)
+    game = SnakeGame()
+    game.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
